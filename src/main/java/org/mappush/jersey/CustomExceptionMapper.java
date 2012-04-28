487716e4d6ab8362abedd9bdaa1f8a16b39a8776
Nicolas Colomer <ncolomer@octo.com>
2012-04-28 18:05:34 +0200

    Add a custom Jersey's ExceptionMapper

---
+++ src/main/java/org/mappush/jersey/CustomExceptionMapper.java
@@ -0,0 +1,18 @@
+package org.mappush.jersey;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CustomExceptionMapper implements ExceptionMapper<Throwable> {
+
+	@Override
+	public Response toResponse(Throwable exception) {
+		return Response.status(Response.Status.BAD_REQUEST)
+				.entity(exception.getMessage())
+				.type("text/plain")
+				.build();
+	}
+
+}
